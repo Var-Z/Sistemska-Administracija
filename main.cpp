53983131adaee53b3eecc8db91781e3d74acc5a9
Žiga Varl <ziga.varl@student.um.si>
2026-03-22 21:11:50 +0100

    countinSort full

--- main.cpp
+++ main.cpp
@@ -5,7 +5,45 @@
 
 using namespace std;
 
-vector<int> countingSort(vector<int> A) {}
+vector<int> countingSort(vector<int> A) {
+    if (A.empty()) return A;
+
+    int min = A[0];
+    int max = A[0];
+
+    for (int i = 1; i < (int)A.size(); i++) {
+        if (A[i] < min) min = A[i];
+        if (A[i] > max) max = A[i];
+    }
+
+    for (int i = 0; i < (int)A.size(); i++) {
+        A[i] = A[i] - min;
+    }
+
+    int range = max - min + 1;
+
+    vector<int> C(range, 0);
+    vector<int> B(A.size());
+
+    for (int i = 0; i < (int)A.size(); i++) {
+        C[A[i]]++;
+    }
+
+    for (int i = 1; i < range; i++) {
+        C[i] = C[i] + C[i - 1];
+    }
+
+    for (int i = (int)A.size() - 1; i >= 0; i--) {
+        B[C[A[i]] - 1] = A[i];
+        C[A[i]] = C[A[i]] - 1;
+    }
+
+    for (int i = 0; i < (int)B.size(); i++) {
+        B[i] = B[i] + min;
+    }
+
+    return B;
+}
 
 
 int main(){
